2e60648c8aaa0e4865188c972573a52d9bec328d
RickSmarr <henriquearaujof8@gmail.com>
2023-08-07 02:01:36 -0300

    chore: adicionado o script e o css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const carro = document.querySelector('.carro');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     carro.classList.add('jump');
@@ -9,8 +10,31 @@ const jump = () => {
 
  }
 
- const loop = setInterval(funcao, 10)
+ const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const carroPosition = +window.getComputedStyle(carro).bottom.replace('px', '');
+
+    if (pipePosition <= 160 && pipePosition > 0 && carroPosition < 70) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        carro.style.animation = 'none';
+        carro.style.bottom = `${carroPosition}px`;
+
+        carro.src = './images/game-over.png';
 
 
+        clearInterval(loop);
 
-document.addEventListener('keydown', jump);
+    }
+
+ }, 10)
+
+
+ document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 32) { // pular somente com o espaço
+      jump();
+    }
+  });
